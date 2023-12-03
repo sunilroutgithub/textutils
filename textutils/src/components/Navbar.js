02ccbdcf4ba84ae1c1400ee55e2633b260ba24cc
sunil_github <117855897+sunilroutgithub@users.noreply.github.com>
2023-12-03 11:18:03 +0530

    Update Navbar.js

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -8,7 +8,8 @@
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
-          <a className="navbar-brand" href="#">{props.title}</a>
+          // <a className="navbar-brand" href="#">{props.title}</a>
+       <Link className="navbar-brand" to="#">{props.title}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
